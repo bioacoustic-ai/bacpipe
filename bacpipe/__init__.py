eeed6a66fe1114e60a5a63d8a2397a8b76e2deaa
vskode <vkather@gmail.com>
2024-11-18 15:06:28 +0100

    #6 add zip extraction to init

--- bacpipe/__init__.py
+++ bacpipe/__init__.py
@@ -1,5 +1,14 @@
 import logging
 
+# Unzip models_example.zip to initiate models dir structure
+import zipfile
+from pathlib import Path
+
+if not Path("bacpipe/model_checkpoints").exists():
+    with zipfile.ZipFile("bacpipe/model_checkpoints.zip", "r") as zip_ref:
+        zip_ref.extractall("bacpipe")
+
+
 # Initialize Logger
 logger = logging.getLogger("bacpipe")
 c_handler = logging.StreamHandler()
