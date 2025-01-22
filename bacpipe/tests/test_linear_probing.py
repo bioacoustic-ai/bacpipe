adc583aaf53e7bdf004627e5d3f341afe924d3e3
vskode <vkather@gmail.com>
2025-01-22 18:14:50 +0100

    #14 add tests and test files to test dimensionality reduction models

--- bacpipe/tests/test_linear_probing.py
+++ bacpipe/tests/test_linear_probing.py
@@ -1,3 +1,41 @@
-from bacpipe.generate_embeddings import generate_embeddings, Loader, Embedder
+from bacpipe.generate_embeddings import Loader
 import numpy as np
 from pathlib import Path
+from bacpipe.evaluation.classification import evaluate_on_task
+
+audio_dir = Path("bacpipe/evaluation/datasets/audio_test_files")
+embed_dir = Path("bacpipe/evaluation/datasets/embedding_test_files")
+
+embeddings = {}
+
+tasks = ["ID", "spcies", "taxon"]
+
+
+def loader_fn():
+    loader = Loader(
+        audio_dir=audio_dir,
+        dim_reduction_model="umap",
+        check_if_combination_exists=False,
+        model_name="aves",
+        testing=True,
+    )
+    assert loader.files is not None and len(loader.files) > 0
+    return loader
+
+
+# Define the pytest_generate_tests hook to generate test cases
+def pytest_generate_tests(metafunc):
+    if "model" in metafunc.fixturenames:
+        # Generate test cases based on the test_data list
+        metafunc.parametrize("task", tasks)
+
+
+def test_task_evaluation(task):
+    loader = loader_fn()
+    # for task in tasks:
+    overall_metrics, per_class_metrics, items_per_class = evaluate_on_task(
+        task, "avesecho_passt", loader, testing=True
+    )
+
+
+test_task_evaluation("ID")
