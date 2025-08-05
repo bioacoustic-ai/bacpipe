7e65b99e610b56875f4fa7773353cb09cbe748cc
vskode <vkather@gmail.com>
2025-08-05 14:40:39 +0200

    more anomalous stuff

--- generate_anomalous_data.py
+++ generate_anomalous_data.py
@@ -1,3 +1,20 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+
+src = "/mnt/swap/Work/Data/Amphibians/AnuranSet/annotations.csv"
+
+annots = pd.read_csv(src)
+
+labs, cnts = np.unique(annots.species, return_counts=True)
+
+labs = labs[cnts < 20]
+
+anomals = annots[annots.species.isin(labs)]
+
+dd = {}
+
+for lab in labs:
+    dd[lab] = np.unique(anomals[anomals.species == lab].wavfilename, return_counts=True)
+
+anomals.to_csv(Path(src).parent.joinpath("anomals.csv"))
