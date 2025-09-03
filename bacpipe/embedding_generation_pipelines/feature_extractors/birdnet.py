208e1f4defe914583e0bfd6b6fbd9cb66649af0c
vskode <vkather@gmail.com>
2025-09-03 18:02:22 +0200

    #30 add birdnet class names file to model_specific_utils

--- bacpipe/embedding_generation_pipelines/feature_extractors/birdnet.py
+++ bacpipe/embedding_generation_pipelines/feature_extractors/birdnet.py
@@ -15,7 +15,7 @@ class Model(ModelBaseClass):
             self.model_base_path / "birdnet", compile=False
         )
         all_classes = pd.read_csv(
-            self.model_base_path / "birdnet/BirdNET_GLOBAL_6K_V2.4_Labels_en_uk.txt",
+            "bacpipe/model_specific_utils/birdnet/BirdNET_GLOBAL_6K_V2.4_Labels_en_uk.txt",
             header=None,
         )
         self.classes = [s.split("_")[-1] for s in all_classes.values.squeeze()]
