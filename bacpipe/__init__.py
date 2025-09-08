e9d2677fce6aa2c9872661cdd995a1e9ca54c245
vskode <vkather@gmail.com>
2025-09-08 19:32:53 +0200

    #20 more changes for packaging, expose pipeline

--- bacpipe/__init__.py
+++ bacpipe/__init__.py
@@ -24,3 +24,50 @@ c_format = logging.Formatter("%(name)s::%(levelname)s:%(message)s")
 c_handler.setFormatter(c_format)
 logger.addHandler(c_handler)
 logger.setLevel(logging.WARNING)
+
+## Expose common functions
+
+import yaml
+from pathlib import Path
+import importlib.resources as pkg_resources
+
+import bacpipe
+from bacpipe.main import (
+    get_model_names,
+    evaluation_with_settings_already_exists,
+    model_specific_embedding_creation,
+    model_specific_evaluation,
+    cross_model_evaluation,
+    visualize_using_dashboard,
+)
+
+with open(bacpipe.PACKAGE_ROOT / "config.yaml") as f:
+    config = yaml.load(f, Loader=yaml.CLoader)
+
+with pkg_resources.open_text(bacpipe, "settings.yaml") as f:
+    settings = yaml.load(f, Loader=yaml.CLoader)
+
+
+def run(config, settings):
+    overwrite, dashboard = config["overwrite"], config["dashboard"]
+
+    if not Path(config["audio_dir"]).exists():
+        raise FileNotFoundError(
+            f"Audio directory {config['audio_dir']} does not exist. Please check the path. "
+            "It should be in the format 'C:\\path\\to\\audio' on Windows or "
+            "'/path/to/audio' on Linux/Mac. But be sure to use single quotes '!"
+        )
+
+    get_model_names(**config, **settings)
+
+    if overwrite or not evaluation_with_settings_already_exists(**config, **settings):
+
+        loader_dict = model_specific_embedding_creation(**config, **settings)
+
+        model_specific_evaluation(loader_dict, **config, **settings)
+
+        cross_model_evaluation(**config, **settings)
+
+    if dashboard:
+
+        visualize_using_dashboard(**config, **settings)
